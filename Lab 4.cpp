83532c564ec5f238cec2bd9ea2ddee76296f7e18
jshaw11uca <jshaw11@cub.uca.edu>
2026-09-23 12:46:50 -0500

    code update

--- Lab 4.cpp
+++ Lab 4.cpp
@@ -1,9 +1,26 @@
 
 
 #include <iostream>
+#include <string>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    string food_name;
+    char code;
+    int quantity;
+
+
+    cout << "Enter the food name: ";
+    getline(cin, food_name);
+
+    cout << "Enter the item code: ";
+    getline(cin, code);
+
+    cout << "How many fruits: ";
+    getline(cin, quantity)
+
+    
+
 }
 
