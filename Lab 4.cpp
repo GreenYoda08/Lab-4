9b68975877ea13e9a8860fbeffb1f967d9ad4ff4
jshaw11uca <jshaw11@cub.uca.edu>
2026-09-23 13:16:53 -0500

    feat: Add basic reciept ouput and input formatting

--- Lab 4.cpp
+++ Lab 4.cpp
@@ -2,6 +2,7 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
 
 int main()
@@ -9,16 +10,34 @@ int main()
     string food_name;
     char code;
     int quantity;
+    double price;
+    char member;
+    string yeno;
 
 
     cout << "Enter the food name: ";
     getline(cin, food_name);
 
     cout << "Enter the item code: ";
-    getline(cin, code);
+    cin >> code;
 
     cout << "How many fruits: ";
-    getline(cin, quantity)
+    cin >> quantity;
+
+    cout << "What is the price: ";
+    cin >> price;
+
+    cout << "Are you a member(y/n): ";
+    cin >> member;
+
+
+
+
+    cout << right << setw(9) << food_name << endl;
+    cout << right << setw(7) << code << endl;
+    cout << right << setw(7) << quantity << endl;
+    cout << right << setw(9) << setprecision(2) << fixed << price << endl;
+    cout << right << setw(7) << member << endl;
 
     
 
